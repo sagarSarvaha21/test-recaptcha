bf89a075562916561e00ae471cf246b91bc18333
shiv-pethealth <shiv.alkondwar@pethealthinc.com>
2022-10-19 00:04:30 +0530

    First Commit

---
+++ src/component/Recaptcha.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import ReCAPTCHA from "react-google-recaptcha";q
+
+
+export default class Recaptcha extends React.Component{
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <>Hello friends!</>
+    )
+  }
+}
